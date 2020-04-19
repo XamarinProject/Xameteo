4361af201cbc5c3eaf26ae303f4c1cebad6e16c5
Alex <alexandrearmando@hotmail.fr>
2020-04-19 18:57:13 +0200

    Clean and add weather page

---
+++ Xameteo/Views/Templates/PrevisionItemTemplate.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xameteo.Views.Templates
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PrevisionItemTemplate : ContentPage
+    {
+        public PrevisionItemTemplate()
+        {
+            //InitializeComponent();
+        }
+    }
+}
