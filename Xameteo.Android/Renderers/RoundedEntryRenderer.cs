c7a836c42cee0166764fc94543b0b7da35ebf0e0
Alex <alexandrearmando@hotmail.fr>
2020-04-24 16:43:17 +0200

    Fix api key + renderer

--- Xameteo.Android/Renderers/RoundedEntryRenderer.cs
+++ Xameteo.Android/Renderers/RoundedEntryRenderer.cs
@@ -1,25 +1,34 @@
-﻿using Android.Content;
+﻿using System;
+using Android.Content;
+using Android.Graphics.Drawables;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 using Xameteo;
 using Xameteo.Droid;
 
-[assembly: ExportRenderer(typeof(RoundedEntry), typeof(RoundedEntryRenderer))]
+[assembly: ExportRenderer(typeof(Xameteo.RoundedEntry), typeof(Xameteo.Droid.RoundedEntryRenderer))]
 namespace Xameteo.Droid
 {
     public class RoundedEntryRenderer : EntryRenderer
     {
         public RoundedEntryRenderer(Context context) : base(context)
         {
-
         }
-
-        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        protected override void
+        OnElementChanged(ElementChangedEventArgs<Entry> e)
         {
             base.OnElementChanged(e);
             if (e.OldElement == null)
             {
-                Control.SetBackgroundResource(Resource.Layout.rounded_shape);
+                var gradientDrawable = new GradientDrawable();
+                gradientDrawable.SetCornerRadius(60f);
+                gradientDrawable.SetStroke(5,
+                Android.Graphics.Color.Aqua);
+                gradientDrawable.SetColor(Android.Graphics.Color.LightGray);
+                Control.SetBackground(gradientDrawable);
+                Control.SetPadding(50, Control.PaddingTop,
+                Control.PaddingRight,
+                Control.PaddingBottom);
             }
         }
     }
